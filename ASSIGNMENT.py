369b69f73c099531d5b6b6565e15eddb7e605cdf
Emmanuel K. Sikavi <74657558+EmmaSik@users.noreply.github.com>
2020-12-13 16:53:57 -0800

    Add files via upload
    Assignment by Emmanuel

---
+++ ASSIGNMENT.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ASSIGNMENT
+
+# In[ ]:
+
+
+
+
+
+# # QUESTION ONE
+
+# In[41]:
+
+
+Ek_msg1 = "EmmaSik: Hello! Goldson, will you attend the data analysis workshop next week?"
+print(Ek_msg1)
+
+Gd_res1 = "Goldson: Yes, I will"
+print(Gd_res1)
+
+Ek_msg2 = "EmmaSik: Alright. See you soon"
+print(Ek_msg2)
+
+Gd_res2 = "Goldson: Thank you"
+print(Gd_res2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# # QUESTION TWO
+
+# In[45]:
+
+
+print ('Jesus said in John 10:10, "The thief cometh not but to steal, to kill and to destroy') 
+print ('but I[JESUS] have come that they might have life, and have it abundantly."')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# # QUESTION THREE
+
+# In[28]:
+
+
+name = "EmmaSik"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+
+# In[ ]:
+
+
+
+
